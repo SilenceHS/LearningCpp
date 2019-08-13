0e716b8a94339c980b8076c411af956c7610ffdc
hangfeifu <5724924@qq.com>
2019-08-13 22:46:11 +0800

    sizeof short int long and long long

---
+++ ch1.Integer/Integer.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    std::cout<<sizeof(short)<<std::endl;//2
+    std::cout<<sizeof(int)<<std::endl;//4
+    std::cout<<sizeof(long)<<std::endl;//4
+    std::cout<<sizeof(long long)<<std::endl;//8
+}
