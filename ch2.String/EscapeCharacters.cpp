fbdc1826b665b242f5036fb1009c3207905c1040
hangfeifu <5724924@qq.com>
2019-08-13 23:01:45 +0800

    Escape Characters

---
+++ ch2.String/EscapeCharacters.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    std::cout<<"hello\n";
+    std::cout<<"aaa\thello"<<std::endl;
+    std::cout<<"aa\thello"<<std::endl;
+    std::cout<<"aa\t\thello"<<std::endl;
+    std::cout<<"aaaaaa\thello"<<std::endl;
+    std::cout<<"aaaaaaaa\thello"<<std::endl;
+    std::cout<<"aaaaaaaaa\thello"<<std::endl;
+}
